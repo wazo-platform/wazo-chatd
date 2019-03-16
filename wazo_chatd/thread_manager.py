e2b1f40e8cd6161a53137330b9865bcf71eb405e
Pascal Cadotte Michaud <pcm@wazo.io>
2019-03-16 16:16:54 -0400

    refactor: rename a variable

--- wazo_chatd/thread_manager.py
+++ wazo_chatd/thread_manager.py
@@ -9,17 +9,17 @@ logger = logging.getLogger(__name__)
 class ThreadManager(object):
 
     def __init__(self):
-        self._threads_wrapper = []
+        self._thread_wrappers = []
 
     def manage(self, thread_wrapper):
-        self._threads_wrapper.append(thread_wrapper)
+        self._thread_wrappers.append(thread_wrapper)
 
     def start(self):
-        for thread_wrapper in self._threads_wrapper:
+        for thread_wrapper in self._thread_wrappers:
             thread_wrapper.start()
 
     def stop(self):
-        for thread_wrapper in self._threads_wrapper:
+        for thread_wrapper in self._thread_wrappers:
             thread_wrapper.stop()
 
     def __enter__(self):
