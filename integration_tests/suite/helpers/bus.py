b618a14e5e2f499d5f8ac59b7bbb565cdd589181
Francois Blackburn <fblackburn@wazo.community>
2020-03-17 14:11:12 -0400

    fix tenant creation name
    new tenant was never created on chatd database, because of the wrong
    event name
    Since we always call find_or_create(tenant_uuid), there are no real
    impact
    The only issue is when we delete this tenant, if it not
    created, then we have a warning in log

--- integration_tests/suite/helpers/bus.py
+++ integration_tests/suite/helpers/bus.py
@@ -1,4 +1,4 @@
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_test_helpers import bus as bus_helper
@@ -7,7 +7,7 @@ from xivo_test_helpers import bus as bus_helper
 class BusClient(bus_helper.BusClient):
     def send_tenant_created_event(self, tenant_uuid):
         self.publish(
-            {'data': {'uuid': tenant_uuid}, 'name': 'auth_tenant_created'},
+            {'data': {'uuid': tenant_uuid}, 'name': 'auth_tenant_added'},
             'auth.tenants.{}.created'.format(tenant_uuid),
         )
 
