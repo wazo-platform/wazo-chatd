64e7503d29adf7926a5ae50266206720a7c9275d
Alexandre Fournier <afournier@wazo.io>
2020-08-05 11:02:48 -0400

    fix server_default for do_not_disturb

--- alembic/versions/6ba500c45fcc_add_dnd_to_presence.py
+++ alembic/versions/6ba500c45fcc_add_dnd_to_presence.py
@@ -12,9 +12,11 @@ import sqlalchemy as sa
 revision = '6ba500c45fcc'
 down_revision = '543bc8a0045f'
 
+
 def upgrade():
     op.add_column(
-        'chatd_user', sa.Column('do_not_disturb', sa.Boolean, nullable=False, server_default='0')
+        'chatd_user',
+        sa.Column('do_not_disturb', sa.Boolean, nullable=False, server_default='false'),
     )
 
 
