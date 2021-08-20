dbd99922819b27c651a8814300e9cd6962924dfa
Ben Boyle <me@benboyle.ca>
2021-08-20 14:58:27 -0300

    implement second test to show that unauthed requests get a 401

--- integration_tests/suite/test_config.py
+++ integration_tests/suite/test_config.py
@@ -1,7 +1,8 @@
 # Copyright 2019-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from hamcrest import assert_that, has_key
+from hamcrest import assert_that, has_key, calling, has_properties
+from xivo_test_helpers.hamcrest.raises import raises
 
 from .helpers.base import (
     APIIntegrationTest,
@@ -10,6 +11,8 @@ from .helpers.base import (
     CHATD_TOKEN_UUID,
 )
 
+from wazo_chatd_client.exceptions import ChatdError
+
 
 @use_asset('base')
 class TestConfig(APIIntegrationTest):
@@ -17,3 +20,10 @@ class TestConfig(APIIntegrationTest):
         chatd_client = APIAssetLaunchingTestCase.make_chatd(CHATD_TOKEN_UUID)
         result = chatd_client.config.get()
         assert_that(result, has_key('rest_api'))
+
+    def test_restrict_only_master_tenant(self):
+        chatd_client = APIAssetLaunchingTestCase.make_chatd()
+        assert_that(
+            calling(chatd_client.config.get),
+            raises(ChatdError, has_properties('status_code', 401)),
+        )
