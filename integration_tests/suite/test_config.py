4eea6a423aa519928cae7baa014481fe1b90c4dc
Ben Boyle <me@benboyle.ca>
2021-08-23 17:05:06 -0300

    implement last test

--- integration_tests/suite/test_config.py
+++ integration_tests/suite/test_config.py
@@ -1,7 +1,9 @@
 # Copyright 2019-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import requests
 from hamcrest import assert_that, has_key, calling, has_properties
+from xivo_test_helpers import until
 from xivo_test_helpers.hamcrest.raises import raises
 
 from .helpers.base import (
@@ -27,3 +29,31 @@ class TestConfig(APIIntegrationTest):
             calling(chatd_client.config.get),
             raises(ChatdError, has_properties('status_code', 401)),
         )
+
+    def test_restrict_on_with_slow_wazo_auth(self):
+        APIAssetLaunchingTestCase.stop_service('chatd')
+        APIAssetLaunchingTestCase.stop_service('auth')
+        APIAssetLaunchingTestCase.start_service('chatd')
+
+        chatd_client = APIAssetLaunchingTestCase.make_chatd(CHATD_TOKEN_UUID)
+
+        def _returns_503():
+            try:
+                chatd_client.config.get()
+            except ChatdError as e:
+                assert e.status_code == 503
+            except requests.RequestException as e:
+                raise AssertionError(e)
+
+        until.assert_(_returns_503, tries=10)
+
+        APIAssetLaunchingTestCase.start_service('auth')
+
+        def _not_return_503():
+            try:
+                response = chatd_client.config.get()
+                assert_that(response, has_key('debug'))
+            except Exception as e:
+                raise AssertionError(e)
+
+        until.assert_(_not_return_503, tries=10)
