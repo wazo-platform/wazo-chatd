fed965d239aa1b56b3fa288592637fab8d7b7826
Julien Alie <jalie@wazo.io>
2022-01-31 09:53:12 -0500

    Adds tenant_uuid to bus event headers
    Why:
    
    * By injecting tenant_uuid into bus events,
    we make it possible to filter messages by
    tenant to minimize leakage by the bus

--- wazo_chatd/plugins/rooms/notifier.py
+++ wazo_chatd/plugins/rooms/notifier.py
@@ -1,4 +1,4 @@
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_bus.resources.chatd.events import (
@@ -15,16 +15,26 @@ class RoomNotifier:
 
     def created(self, room):
         room_json = RoomSchema().dump(room)
+        tenant_uuid = room_json['tenant_uuid']
         for user in room_json['users']:
             event = UserRoomCreatedEvent(user['uuid'], room_json)
             self._bus.publish(
-                event, headers={'user_uuid:{uuid}'.format(uuid=user['uuid']): True}
+                event,
+                headers={
+                    'user_uuid:{uuid}'.format(uuid=user['uuid']): True,
+                    'tenant_uuid': tenant_uuid,
+                },
             )
 
     def message_created(self, room, message):
         message_json = MessageSchema().dump(message)
+        tenant_uuid = message_json['tenant_uuid']
         for user in room.users:
             event = UserRoomMessageCreatedEvent(user.uuid, room.uuid, message_json)
             self._bus.publish(
-                event, headers={'user_uuid:{uuid}'.format(uuid=user.uuid): True}
+                event,
+                headers={
+                    'user_uuid:{uuid}'.format(uuid=user.uuid): True,
+                    'tenant_uuid': tenant_uuid,
+                },
             )
