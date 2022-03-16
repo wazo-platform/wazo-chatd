643fa2c1cc9aec66c5bc5d6e519ef1d9d2df6fcd
Julien Alie <jalie@wazo.io>
2022-03-16 15:23:20 -0400

    Status: Modify test to check bus_consumer status

--- integration_tests/suite/test_status.py
+++ integration_tests/suite/test_status.py
@@ -1,7 +1,7 @@
-# Copyright 2019-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from hamcrest import assert_that, has_entries
+from hamcrest import assert_that, has_entries, has_entry
 from wazo_test_helpers import until
 
 from .helpers.base import APIIntegrationTest, use_asset
@@ -12,6 +12,12 @@ class TestStatusAllOK(APIIntegrationTest):
     def test_when_status_then_status_ok(self):
         def status_ok():
             result = self.chatd.status.get()
-            assert_that(result['rest_api'], has_entries({'status': 'ok'}))
+            assert_that(
+                result,
+                has_entries(
+                    rest_api=has_entry('status', 'ok'),
+                    bus_consumer=has_entry('status', 'ok'),
+                ),
+            )
 
         until.assert_(status_ok, timeout=5)
