224c589ce97a256e3e3f9b80599d14b22264b16e
Julien Alie <jalie@wazo.io>
2022-09-02 20:33:06 -0400

    simplify bus naming and parameters

--- wazo_chatd/bus.py
+++ wazo_chatd/bus.py
@@ -3,10 +3,21 @@
 
 from xivo.status import Status
 from xivo_bus.consumer import BusConsumer as BaseConsumer
+from xivo_bus.publisher import BusPublisher as BasePublisher
 
 
 class BusConsumer(BaseConsumer):
+    @classmethod
+    def from_config(cls, bus_config):
+        return cls(name='wazo-chatd', **bus_config)
+
     def provide_status(self, status):
         status['bus_consumer']['status'] = (
             Status.ok if self.consumer_connected() else Status.fail
         )
+
+
+class BusPublisher(BasePublisher):
+    @classmethod
+    def from_config(cls, service_uuid, bus_config):
+        return cls(name='wazo-chatd', service_uuid=service_uuid, **bus_config)
