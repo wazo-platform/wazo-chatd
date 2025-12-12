9e138033ad9d5e3155e97918fe121a371c23da8f
Charles Langlois <charles.langlois@wazo.io>
2025-12-12 17:50:41 -0500

    sqlalchemy: avoid using Query.from_self

--- wazo_chatd/database/helpers.py
+++ wazo_chatd/database/helpers.py
@@ -1,10 +1,19 @@
-# Copyright 2019-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2025 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
+from __future__ import annotations
 
 from contextlib import contextmanager
+from typing import TYPE_CHECKING, TypeVar
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.orm import Query, scoped_session, sessionmaker
+
+if TYPE_CHECKING:
+    # NOTE(clanglois): can be removed in sqlalchemy 2.0
+    from sqlalchemy_stubs import Query  # type: ignore[no-redef] # noqa: F811
+
+    T = TypeVar('T')
+
 
 Session = scoped_session(sessionmaker())
 
@@ -25,3 +34,12 @@ def session_scope():
         raise
     finally:
         Session.remove()
+
+
+def get_query_main_entity(query: Query[T]) -> T:
+    """
+    Returns the main target entity of the query,
+    given it may change while building the query (e.g. aliased)
+    """
+    assert query.column_descriptions
+    return query.column_descriptions[0]['entity']
